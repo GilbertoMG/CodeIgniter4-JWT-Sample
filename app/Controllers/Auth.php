38330c5bcd4b552839aefa5e7880756932904b3d
GilbertoMG <gilbmg@gmail.com>
2021-03-01 18:53:00 -0300

    ajustes PSR

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -2,45 +2,44 @@
 namespace App\Controllers;
 
 use Firebase\JWT\JWT;
-
 use App\Controllers\Base;
 
 class Auth extends Base
 {
-	
-	public function index()
-    {
-		return $this->failUnauthorized('Acesso negado');
-    
-	}	
-	
-	public function create()
-	{
-			
-		$dados = $this->request->getJSON();
-				
-		// Chama o model da autenticação e retorna se válido ou não
-		
-		if ($dados->email == 'gilbmg@gmail.com' && $dados->senha == '123') {
-			
-			$key = API_KEY;
-			
-			$payload = [
-				'aud' => 'http://192.168.56.1/estudo-api/api2/public',
-				'iat' => time(),
-				'nbf' => strtotime('NOW'), // não válido antes de
-				'exp' => strtotime('+3 WEEK') // Não válido após
-			];
 
-		    $jwt = JWT::encode($payload, $key);
-			
-			return $this->respond(['userName'=>'Gilberto','token' => $jwt],201);
-			
-		} else {
-			
-			return $this->respond(['messages' => 'Credenciais inválidas'],401);
-			
-		}		
-	}	
-    
+    public function index()
+    {
+        return $this->failUnauthorized('Acesso negado');
+    }
+
+    public function create()
+    {
+        $dados = $this->request->getJSON();
+
+        // Chama o model da autenticação e retorna se válido ou não
+
+        if ($dados->email == 'gilbmg@gmail.com' && $dados->senha == '123') {
+
+            $key = API_KEY;
+
+            $payload = [
+                'aud' => 'http://192.168.56.1/estudo-api/api2/public',
+                'iat' => time(),
+                'nbf' => strtotime('NOW'), // não válido antes de
+                'exp' => strtotime('+3 WEEK') // Não válido após
+            ];
+
+            $jwt = JWT::encode($payload, $key);
+
+            return $this->respond([
+                'userName' => 'Gilberto',
+                'token' => $jwt
+            ], 201);
+        } else {
+
+            return $this->respond([
+                'messages' => 'Credenciais inválidas'
+            ], 401);
+        }
+    }
 }
